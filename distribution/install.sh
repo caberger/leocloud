16d5d0f39d70b1b8f4750bb996ebab71f9db414b
Christian Aberger <qrzbrz@yahoo.de>
2021-10-14 19:48:42 +0200

    install script

---
+++ distribution/install.sh
@@ -0,0 +1,10 @@
+#/usr/bin/env bash
+
+# this script is used by github actions to copy the compiled binaries to the destination server
+
+GITHUB_USER=$1
+ACCESS_TOKEN=$2
+
+echo "user $GITHUB_USER"
+docker login ghcr.io -u $GITHUB_USER -p $ACCESS_TOKEN
+
