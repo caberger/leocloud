d4ba75989d6a1df105235cf1a11045a459a39d1d
Christian Aberger <qrzbrz@yahoo.de>
2021-10-14 21:21:12 +0200

    prune images

--- distribution/install.sh
+++ distribution/install.sh
@@ -7,10 +7,16 @@ ACCESS_TOKEN=$2
 IMAGE=$3
 
 # systemctl stop docker-compose.service
+
 docker container prune -f
-docker image prune -f
+
+
 docker login ghcr.io -u $GITHUB_USER -p $ACCESS_TOKEN
 docker pull $IMAGE-appsrv:latest
+
+echo "prune images..."
+
+docker image prune -f
 docker image ls
 
 #systemctl start docker-compose.service
