69d3f66cccf43c8433d10225b4aee0f4b4b76c39
Christian Aberger <qrzbrz@yahoo.de>
2021-10-14 20:12:11 +0200

    pull

--- distribution/install.sh
+++ distribution/install.sh
@@ -4,7 +4,10 @@
 
 GITHUB_USER=$1
 ACCESS_TOKEN=$2
+IMAGE=$3
 
-echo "user=$GITHUB_USER token=$ACCESS_TOKEN"
 docker login ghcr.io -u $GITHUB_USER -p $ACCESS_TOKEN
+docker pull $IMAGE-appsrv:latest
+docker image ls
+
 
