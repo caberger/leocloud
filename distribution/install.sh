c2e08f5b4ffd03e70d58c27547bc835e87d74c61
Christian Aberger <qrzbrz@yahoo.de>
2021-10-14 20:28:16 +0200

    install docker images remote

--- distribution/install.sh
+++ distribution/install.sh
@@ -1,13 +1,13 @@
 #/usr/bin/env bash
 
-# this script is used by github actions to copy the compiled binaries to the destination server
+# this script is copied to the server and executed on the remote server for installation
 
 GITHUB_USER=$1
 ACCESS_TOKEN=$2
 IMAGE=$3
 
-ssh server "sudo docker login ghcr.io -u $GITHUB_USER -p $ACCESS_TOKEN"
-ssh server "sudo docker pull $IMAGE-appsrv:latest"
-ssh server "sudo docker image ls"
+docker login ghcr.io -u $GITHUB_USER -p $ACCESS_TOKEN
+docker pull $IMAGE-appsrv:latest
+docker image ls
 
 
