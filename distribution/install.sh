afe00b672b0c07e439eb13168aff60d1038e7778
Christian Aberger <qrzbrz@yahoo.de>
2021-10-16 11:43:52 +0200

    fix sudo command

--- distribution/install.sh
+++ distribution/install.sh
@@ -9,7 +9,8 @@ SERVER_USER=$4
 
 # systemctl stop docker-compose.service
 
-docker container prune -f
+docker container prune --force
+docker image prune --force
 
 docker login ghcr.io -u $GITHUB_USER -p $ACCESS_TOKEN
 docker pull $IMAGE-appsrv:latest
@@ -20,7 +21,7 @@ docker tag $IMAGE-nginx:latest leo-nginx
 
 echo "prune images..."
 
-docker image prune -f
+docker image prune -force
 docker image ls
 
 #systemctl start docker-compose.service
