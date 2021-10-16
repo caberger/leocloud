542b0044dd78eff274e8bad4c04f3ec6a6ced60e
Christian Aberger <qrzbrz@yahoo.de>
2021-10-16 12:32:10 +0200

    restart docker

--- docker/build.sh
+++ docker/build.sh
@@ -1,25 +1,34 @@
 #!/usr/bin/env bash
 
+# this script is copied to the server and executed on the remote server for installation
+
+GITHUB_USER=$1
+ACCESS_TOKEN=$2
+IMAGE=$3
+SERVER_USER=$4
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-REGISTRY=$2
-IMAGES="appsrv nginx"
-pushd $SCRIPT_DIR
+echo "dir $SCRIPT_DIR"
+mkdir -p /usr/local/bin/application
 
-if [[ "$1." == "build." ]]
-then
-    for image in $IMAGES
-    do
-        docker build $image --tag $REGISTRY-$image || exit 3
-    done
-elif [[ "$1." == "push." ]]
-then
-    for image in $IMAGES
-    do
-        docker push $REGISTRY-$image || exit 2
-    done
-else
-    echo "usage: $0 build|push <username>"
-    exit 1
-fi
+pushd /usr/local/bin/application
+pwd
+cp $SCRIPT_DIR/* .
 
+docker-compose down
+docker container prune --force
+docker image prune --force
+
+docker login ghcr.io -u $GITHUB_USER -p $ACCESS_TOKEN
+docker pull $IMAGE-appsrv:latest
+docker tag $IMAGE-appsrv:latest leo-appsrv
+
+docker pull $IMAGE-nginx:latest
+docker tag $IMAGE-nginx:latest leo-nginx
+
+echo "prune images..."
+
+docker image prune -force
+docker image ls
+docker-compose up --detach
 popd
