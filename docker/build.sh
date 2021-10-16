79881b72435c586633b4cede09d6f281e586d76e
Christian Aberger <qrzbrz@yahoo.de>
2021-10-16 11:23:32 +0200

    style home page

--- docker/build.sh
+++ docker/build.sh
@@ -9,13 +9,13 @@ if [[ "$1." == "build." ]]
 then
     for image in $IMAGES
     do
-        docker build $image --tag $REGISTRY/$image || exit 3
+        docker build $image --tag $REGISTRY-$image || exit 3
     done
 elif [[ "$1." == "push." ]]
 then
     for image in $IMAGES
     do
-        docker push $REGISTRY/$image || exit 2
+        docker push $REGISTRY-$image || exit 2
     done
 else
     echo "usage: $0 build|push <username>"
