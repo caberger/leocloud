5949ef3e4accaa6bec71f3b51aed1ee27040ad8e
qrzbrz <qrzbrz@yahoo.de>
2021-10-18 15:09:23 +0200

    add module

---
+++ docker/nginx/html/src/index.js
@@ -0,0 +1,6 @@
+/** example javascript module */
+
+window.addEventListener("DOMContentLoaded", e => {
+    let button = document.getElementById("github")
+    button.onclick = window.location.url = "https://github.com/caberger/leocloud"
+})
