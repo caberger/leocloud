ad9f3b09ec4b4c49df8f3e86a2cc7f676fb33ab4
caberger <qrzbrz@yahoo.de>
2021-10-23 13:48:33 +0200

    build with webpack

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,7 +1,6 @@
 const { resolve } = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-//const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require("copy-webpack-plugin")
 const _ = require("lodash")
 
