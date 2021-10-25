bb4f6209a16785adef2d451462f0e09076ed9d78
Christian Aberger <qrzbrz@yahoo.de>
2021-10-25 21:14:05 +0200

    revert to docker-compose 3.3

--- docker/appsrv/scripts/run-native.sh
+++ docker/appsrv/scripts/run-native.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-RUNNER=$(ls *-runner)
+while ! nc -z mysql 3306; do   
+    echo "waiting for godot..."
+    sleep 1
+done
 
-#while ! nc -z mysql 3306; do   
-#    echo "waiting for godot..."
-#    sleep 1
-#done
-
-echo "starting quarkus $RUNNER..."
-./$RUNNER
+echo "starting quarkus..."
+pwd
+ls -l
+./application
 
