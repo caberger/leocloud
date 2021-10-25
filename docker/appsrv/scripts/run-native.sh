9ecc835cf81697d2e78869b7bfc973411309c48d
Christian Aberger <qrzbrz@yahoo.de>
2021-10-25 19:16:08 +0200

    healthcheck

--- docker/appsrv/scripts/run-native.sh
+++ docker/appsrv/scripts/run-native.sh
@@ -2,13 +2,11 @@
 
 RUNNER=$(ls *-runner)
 
-while ! nc -z mysql 3306; do   
-    echo "waiting for godot..."
-    sleep 1
-done
+#while ! nc -z mysql 3306; do   
+#    echo "waiting for godot..."
+#    sleep 1
+#done
 
 echo "starting quarkus $RUNNER..."
-pwd
-ls -l
 ./$RUNNER
 
