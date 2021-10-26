29eaff32f01c0d5cd7e93d4cc668c7f3f78a1898
Christian Aberger <qrzbrz@yahoo.de>
2021-10-26 18:16:12 +0200

    server-setup.sh

---
+++ distribution/server-setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# This script is executed on the server. It installs required packages
+# It is used to prepare an Canonical Ubuntu Minimal Server to run our application
+# requirements: the user must be able to execute sudo without a password
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+MARKER_FILE="already-installed.txt"
+DOCKER_USER=$1
+
+if [[ -f $MARKER_FILE]]
+then
+    echo "$MARKER_FILE exists, do nothing"
+else
+    apt -y update || (echo "update failed" && exit 1)
+    apt -y install docker-compose net-tools ||  (echo "installation failed" && exit 2)
+    usermod -aG docker $DOCKER_USER || exit 3
+    echo "package installation successful" > $MARKER_FILE
+fi
+exit
+
