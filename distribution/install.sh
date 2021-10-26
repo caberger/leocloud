c454729e9e4e12fb41000fba1f5919ad79721037
Christian Aberger <qrzbrz@yahoo.de>
2021-10-26 10:13:03 +0200

    dockre-compose service on remote server

--- distribution/install.sh
+++ distribution/install.sh
@@ -10,14 +10,24 @@ IMAGE=$3
 SERVER_USER=$4
 REGISTRY_ESCAPED=$(echo $IMAGE| sed -e "s/\//\\\\\//g")
 
+SERVICE_SCRIPT=docker-compose.service
+SERVICE_SCRIPT_DESTINATION=/lib/systemd/system/$SERVICE_SCRIPT
+
 mkdir -p /usr/local/bin/application
 pushd /usr/local/bin/application
 pwd
 cp $SCRIPT_DIR/* .
 
+if [[ ! -f $SERVICE_SCRIPT ]]
+then
+    cp $SCRIPT_DIR/server/*.service $SERVICE_SCRIPT_DESTINATION
+    systemctl enable SERVICE_SCRIPT
+fi
+
 sed -e "s/image\:\s*leo\-\(.*\)$/image\: ${REGISTRY_ESCAPED}\-\1/g" docker-compose-production.yml > docker-compose.yml
 
-systemctl stop docker-compose || echo "docker-compose service not installed"
+systemctl stop docker-compose || echo "docker-compose service not active"
+
 docker-compose down
 systemctl restart docker # <-- ensure no ports have remained open
 netstat -ant
