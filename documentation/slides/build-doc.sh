6b1625c9b46b5a9f3a0ae580774d971f8085db69
Christian Aberger <qrzbrz@yahoo.de>
2021-11-01 19:24:54 +0100

    use uid and gid

--- documentation/slides/build-doc.sh
+++ documentation/slides/build-doc.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-UID=$(id -u)
-GID=$(id -g)
+#UID=$(id -u)
+#GID=$(id -g)
+
+echo "UID=$UID, GID=$GID"
 
 docker run --rm -v $(pwd):/documents/ --user $UID:$GID asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-diagram -a docinfo=shared -a linkcss -a copycss -a lang=de -D html index.adoc
 cp -r css/ images/ html/
