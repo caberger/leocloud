0c5b2c5698895e94d81234b0bee9a8e7338dd3fd
Christian Aberger <qrzbrz@yahoo.de>
2021-11-01 15:12:05 +0100

    fix uid and gid

--- documentation/slides/build-doc.sh
+++ documentation/slides/build-doc.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-docker run --rm -v $(pwd):/documents/ --user 1000:1000 asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-diagram -a docinfo=shared -a linkcss -a copycss index.adoc
+UID=$(id -u)
+GID=$(id -g)
+docker run --rm -v $(pwd):/documents/ --user $UID:$GID asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-diagram -a docinfo=shared -a linkcss -a copycss index.adoc
