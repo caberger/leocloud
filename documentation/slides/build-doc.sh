f1c3c8909773f60ddeb2fcfe34bc5f02d3f379a9
Christian Aberger <qrzbrz@yahoo.de>
2021-11-01 15:23:06 +0100

    fix build.doc

--- documentation/slides/build-doc.sh
+++ documentation/slides/build-doc.sh
@@ -2,4 +2,6 @@
 
 UID=$(id -u)
 GID=$(id -g)
-docker run --rm -v $(pwd):/documents/ --user $UID:$GID asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-diagram -a docinfo=shared -a linkcss -a copycss index.adoc
+
+docker run --rm -v $(pwd):/documents/ --user $UID:$GID asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-diagram -a docinfo=shared -a linkcss -a copycss -D html index.adoc
+cp -r css/ images/ html/
