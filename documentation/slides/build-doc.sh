c34f0b0b8291d0253952f90815c35277a623941d
Christian Aberger <qrzbrz@yahoo.de>
2021-11-01 19:26:44 +0100

    add gid

--- documentation/slides/build-doc.sh
+++ documentation/slides/build-doc.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-#UID=$(id -u)
-#GID=$(id -g)
+USERID=$(id -u)
+GROUPID=$(id -g)
 
-echo "UID=$UID, GID=$GID"
-
-docker run --rm -v $(pwd):/documents/ --user $UID:$GID asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-diagram -a docinfo=shared -a linkcss -a copycss -a lang=de -D html index.adoc
+echo "UID=$UID, USERID=$USERID, GROUPID=$GROUPID"
+id
+docker run --rm -v $(pwd):/documents/ --user $USERID:$GROUPID asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-diagram -a docinfo=shared -a linkcss -a copycss -a lang=de -D html index.adoc
 cp -r css/ images/ html/
