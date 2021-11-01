d3570a3b680489f3f0e989d2d1a7c738d48a3fc2
Christian Aberger <qrzbrz@yahoo.de>
2021-11-01 15:08:48 +0100

    fix build shell script

--- documentation/slides/build-doc.sh
+++ documentation/slides/build-doc.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -v $$(pwd):/documents/ --user 1000:1000 asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-diagram -a docinfo=shared -a linkcss -a copycss index.adoc
+docker run --rm -v $(pwd):/documents/ --user 1000:1000 asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-diagram -a docinfo=shared -a linkcss -a copycss index.adoc
