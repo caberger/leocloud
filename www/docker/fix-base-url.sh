0a801e9f5657d1f8d753dd4373ae6553c3b5eed3
Christian Aberger <qrzbrz@yahoo.de>
2021-11-02 19:03:02 +0100

    fix href

--- www/docker/fix-base-url.sh
+++ www/docker/fix-base-url.sh
@@ -3,13 +3,13 @@
 # this script changes the <base href="..."> header tag to the environment variable BASE_HREF.
 # then the same docker image can be deployed to multiple sub-paths of the same hostname
 
-HTML=/usr/share/nginx/html
-
+#HTML=/usr/share/nginx/html
+HTML=./target
 if [[ "$BASE_HREF." == "." ]]
 then
     echo "no BASE_HREF environment variable set, keep <base href...> as is is"
 else
     echo "BASE_HREF=$BASE_HREF patch the head to <base href=\"$BASE_HREF\">"
-    find $HTML -type f -name "*.html" -print -exec sed -e "s/<base href=\"/\">/<base href=\"\\/$BASE_HREF\\/\">/" {} \;
+    find $HTML -type f -name "*.html" -print -exec sed -i -e "s,<base href=\"/\",<base href=\"/$BASE_HREF/\"," {} \;
 fi
 
