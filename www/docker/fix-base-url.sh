ca9dbf343208639ae1d0ce042869800709813c7b
Christian Aberger <qrzbrz@yahoo.de>
2021-11-02 17:05:49 +0100

    refactor to BASE_HREF

--- www/docker/fix-base-url.sh
+++ www/docker/fix-base-url.sh
@@ -4,6 +4,7 @@
 # then the same docker image can be deployed to multiple sub-paths of the same hostname
 
 HTML=/usr/share/nginx/html
-echo "fix the baseUrl of all .html files to $BASE_URL"
-find $HTML -type f -name "*.html" -print -exec sed -e -i "s/<base href=\(.*\)>\s*$/<base href=\"\\/$BASE_URL\\/\">/g" {} \;
+echo "fix the baseUrl of all .html files to $BASE_HREF"
+echo find $HTML -type f -name "*.html" -print -exec sed -e -i "s/<base href=\(.*\)>\s*$/<base href=\"\\/$BASE_HREF\\/\">/g" {} \;
+find $HTML -type f -name "*.html" -print -exec sed -e -i "s/<base href=\(.*\)>\s*$/<base href=\"\\/$BASE_HREF\\/\">/g" {} \;
 
