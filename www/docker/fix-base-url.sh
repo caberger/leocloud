d1c3f2bb3e97a57cc7540afe83fa570c52b3c0c8
Christian Aberger <qrzbrz@yahoo.de>
2021-11-02 19:13:49 +0100

    fix log

--- www/docker/fix-base-url.sh
+++ www/docker/fix-base-url.sh
@@ -6,9 +6,9 @@
 HTML=/usr/share/nginx/html
 if [[ "$BASE_HREF." == "." ]]
 then
-    echo "no BASE_HREF environment variable set, keep <base href...> as is is"
+    echo "no BASE_HREF environment variable set, keep base href... as is is"
 else
-    echo "BASE_HREF=$BASE_HREF patch the head to <base href=\"$BASE_HREF\">"
+    echo "BASE_HREF=$BASE_HREF patch the head to base href=\"$BASE_HREF\""
     find $HTML -type f -name "*.html" -print -exec sed -i -e "s,<base href=\"/\",<base href=\"/$BASE_HREF/\"," {} \;
 fi
 
